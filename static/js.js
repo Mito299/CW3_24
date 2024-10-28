d61d1004b812b9c18d99f09f2eebdf1f6e19b2fc
Myto299 <jack299jai299@gmail.com>
2024-10-28 16:50:59 +0700

    เปลี่ยน

--- static/js.js
+++ static/js.js
@@ -6,15 +6,15 @@ function showSchedule() {
     switch (day) {
         case "1":
             scheduleText = `
-                    <p>ຊ/ມ 1: Ex ພາສາອັງກຶດ ຫ້ອງ: (FNS201)</p>
+                    <p>ຊ/ມ 1: Ex ພາສາອັງກິດ ຫ້ອງ: (FNS201)</p>
                     <p>ຊ/ມ 2: lab Mis Access jan ສົມມະນີ້: (CS003)</p>
-                    <p>ຊ/ມ 3: Lect ລະບົບຄອມພຶວເຕີ: (MA201)</p>
+                    <p>ຊ/ມ 3: Lect ລະບົບຄອມພິວເຕີ: (MA201)</p>
                     <p>ຊ/ມ 4: ວ່າງ</p>
                 `;
             break;
         case "2":
             scheduleText = `
-                    <p>ຊ/ມ 1: EX ພາສາອັງກຶດ ຫ້ອງ: (FNS312)</p>
+                    <p>ຊ/ມ 1: EX ພາສາອັງກິດ ຫ້ອງ: (FNS312)</p>
                     <p>ຊ/ມ 2: Led ການນຳໃຊ້ຊອບພື້ນຖານ: (CS003)</p>
                     <p>ຊ/ມ 3: Lect ຄວາມຮູ້ພື້ນຖານກ່ຽວກັບຖານຂໍ້ມູນ: (FNS401)</p>
                     <p>ຊ/ມ 4: Lect java: (FNS402)</p>
@@ -23,7 +23,7 @@ function showSchedule() {
         case "3":
             scheduleText = `
                     <p>ຊ/ມ 1: ວ່າງ</p>
-                    <p>ຊ/ມ 2: Lect ຄະນິດສາດສຳຮັບຄອມພຶວເຕີ: (MA205)</p>
+                    <p>ຊ/ມ 2: Lect ຄະນິດສາດສຳຮັບຄອມພິວເຕີ: (MA205)</p>
                     <p>ຊ/ມ 3: Lad java: (CS005)</p>
                     <p>ຊ/ມ 4: ວ່າງ</p>
                 `;
@@ -31,7 +31,7 @@ function showSchedule() {
         case "4":
             scheduleText = `
                     <p>ຊ/ມ 1: Lect ລາວສຶກສາ1
-                    <p>ຊ/ມ 2: EX ຄະນຶດສາດສຳລັບຄອມພຶວເຕີ: (FNS403)</p>
+                    <p>ຊ/ມ 2: EX ຄະນິດສາດສຳລັບຄອມພິວເຕີ: (FNS403)</p>
                     <p>ຊ/ມ 3: ວ່າງ</p>
                     <p>ຊ/ມ 4: ວ່າງ</p>
                 `;
